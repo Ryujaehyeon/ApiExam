337a7bc595ca92d5aec8b35763dfc72e37b30710
yjhg15 <yjhg15@naver.com>
2019-10-01 20:43:38 +0900

    3장 문자열 출력 3-1 TextOut 3-2 DrawText

--- First/DrawText/First.cpp
+++ First/DrawText/First.cpp
@@ -42,32 +42,20 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
 	PAINTSTRUCT ps;
+	RECT rt = { 100, 100, 400, 300 };
+
+	// const char* 형을 char* 형에 할당할 수 없어 변수 자료형 변경됨 
+	const TCHAR* str = TEXT("너무" "길어서" "생략함. :)"); 
 
 	switch (iMessage)
 	{
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
-	//case WM_LBUTTONDOWN:
-	//	hdc = GetDC(hWnd);
-	//	TextOut(hdc, 100, 100, TEXT("Beautiful Korea"), 15);
-	//	ReleaseDC(hWnd, hdc);
-	//	return 0;
+
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps); 
-		//SetTextAlign(hdc, TA_CENTER);
-		//SetTextAlign(hdc, TA_UPDATECP);
-		//SetTextAlign(hdc, TA_LEFT);
-		//SetTextAlign(hdc, TA_RIGHT);
-		//TextOut(hdc, 200, 60, TEXT("Beautiful Korea"), 15);
-		//TextOut(hdc, 200, 80, TEXT("is My"), 5);
-		//TextOut(hdc, 200, 100, TEXT("Lovely Home Country"), 19);
-
-		TCHAR str[128];
-		int Score = 85;
-		wsprintf(str, TEXT("현재 점수는 %d점 입니다."), Score);
-		TextOut(hdc, 10, 10, str, lstrlen(str));
-
+		DrawText(hdc, str, -1, &rt, DT_CENTER | DT_WORDBREAK);
 		EndPaint(hWnd, &ps);
 		return 0;
 	}
