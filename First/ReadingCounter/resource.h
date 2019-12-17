22b66fefdaabddcac68282d5c99f4221df756386
yjhg15 <yjhg15@naver.com>
2019-12-17 18:05:27 +0900

    8-3-라 독서 도우미

---
+++ First/ReadingCounter/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// ReadingCounter.rc에서 사용되고 있습니다.
+//
+#define IDD_READING                     101
+#define IDC_CHKBEEP                     102
+#define IDC_STTIME                      103
+#define IDC_BTNSTART                    104
+#define IDC_BTNPAUSE                    105
+#define IDC_CANCEL                      106
+#define IDC_EDSTART                     107
+#define IDC_EDEND                       108
+#define IDC_EDTIME                      109
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         109
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
